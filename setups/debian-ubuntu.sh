e5aab2d1c8151fe91e72a64c9f5ff127a0735165
- <->
2026-02-13 13:57:13 +0500

    -

--- setups/debian-ubuntu.sh
+++ setups/debian-ubuntu.sh
@@ -3,10 +3,14 @@ sudo apt install 7zip curl time gcc clang ghc openjdk-25-jdk elixir rustc python
 curl -OSL https://github.com/denoland/deno/releases/download/v2.6.9/deno-x86_64-unknown-linux-gnu.zip
 7z e deno-x86_64-unknown-linux-gnu.zip
 sudo cp deno /usr/bin
+rm ./deno-x86_64-unknown-linux-gnu.zip
+rm ./deno
 #nim
 curl -O https://nim-lang.org/download/nim-2.2.6-linux_x64.tar.xz
 tar -xvf ./nim-2.2.6-linux_x64.tar.xz
-sudo ./install.sh
+sudo ./nim-2.2.6/install.sh
+rm ./nim-2.2.6-linux_x64.tar.xz
+rm -fdr ./nim-2.2.6
 #oha
 echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ stable main" | sudo tee /etc/apt/sources.list.d/azlux.list
 sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg https://azlux.fr/repo.gpg
