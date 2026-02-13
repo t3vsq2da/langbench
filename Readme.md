cf0c47721d654361319749cdc0b4243f653e1a7e
- <->
2026-02-13 15:24:38 +0500

    add setup scripts

--- Readme.md
+++ Readme.md
@@ -1,1 +1,8 @@
+# LangBench — Cross-Platform Programming Language Benchmarking Tool
+
+**LangBench** is a utility for comparing the performance of programs written in different programming languages.  
+It automatically runs benchmarks, measures execution time, memory usage, and other system metrics, then outputs summarized results.
+
+[English](Readme.md) | [Русский](Readme.ru.md)
+[Русский](Readme.ru.md)
 [Русский](Readme.ru.md)
---
+++ setups/fedora.sh
@@ -0,0 +1,19 @@
+sudo dnf install -y time gcc clang ghc cabal-install java-latest-openjdk-devel elixir rust python3 pypy3 nodejs perl golang ocaml cargo
+#deno
+curl -OSL https://github.com/denoland/deno/releases/download/v2.6.9/deno-x86_64-unknown-linux-gnu.zip
+7z e deno-x86_64-unknown-linux-gnu.zip
+sudo cp deno /usr/bin
+rm ./deno-x86_64-unknown-linux-gnu.zip
+rm ./deno
+#nim
+curl -O https://nim-lang.org/download/nim-2.2.6-linux_x64.tar.xz
+tar -xvf ./nim-2.2.6-linux_x64.tar.xz
+cd ./nim-2.2.6
+sudo ./install.sh /usr/bin
+#rm ./nim-2.2.6-linux_x64.tar.xz
+#rm -fdr ./nim-2.2.6
+#oha
+cargo install oha
+sudo ln -sf ~/.cargo/bin/oha /usr/bin/oha
+cabal update
+cabal install --lib network
