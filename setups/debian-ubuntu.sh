fd3bc9aba4c0d94619a9e5f9d68e4ce3ad87e38a
- <->
2026-02-13 16:43:06 +0500

    -

--- setups/debian-ubuntu.sh
+++ setups/debian-ubuntu.sh
@@ -1,4 +1,4 @@
-sudo apt install 7zip curl time gcc clang ghc openjdk-25-jdk elixir rustc python3 pypy3 perl nodejs golang-go ocaml cabal-install
+sudo apt install make 7zip curl time gcc clang ghc openjdk-25-jdk elixir rustc python3 pypy3 perl nodejs golang-go ocaml cabal-install
 #deno
 curl -OSL https://github.com/denoland/deno/releases/download/v2.6.9/deno-x86_64-unknown-linux-gnu.zip
 7z e deno-x86_64-unknown-linux-gnu.zip
@@ -18,6 +18,14 @@ echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packa
 sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg https://azlux.fr/repo.gpg
 sudo apt update
 sudo apt install oha
+#ocaml
+curl -LO https://github.com/ocaml/ocaml/releases/download/5.4.0/ocaml-5.4.0.tar.gz
+tar -xzf ocaml-5.4.0.tar.gz
+cd ocaml-5.4.0
+./configure
+sudo make install
+cd ../
+rm -fdr ocaml-5.4.0
 #haskell
 cabal update
 cabal install --lib network
