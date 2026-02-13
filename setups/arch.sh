e7fa7cca37193bcba5197212ac7357213ec4aaf3
- <->
2026-02-13 14:49:48 +0500

    -

--- setups/arch.sh
+++ setups/arch.sh
@@ -1,4 +1,4 @@
-sudo pacman -S time gcc clang ghc jdk-openjdk elixir rust python pypy3 perl nodejs deno nim go ocaml cabal-install oha
+sudo pacman -S time gcc clang ghc jdk-openjdk elixir rust python pypy3 perl nodejs deno nim go ocaml cabal-install oha ghc-static
 #haskell
 cabal update
-cabal install --lib network
+cabal install --lib network --force-reinstalls
